a48c339b16af73130ab018c61f19a79e3e18ea5d
Kristoffer Westerberg <kwesterb@ug.kth.se>
2026-09-23 16:57:31 +0200

    changed adam optimizer

--- src/optimizers/adam.py
+++ src/optimizers/adam.py
@@ -17,15 +17,18 @@ class Adam(Optimizer):
         self.m = [np.zeros_like(p) for p in params]
         self.v = [np.zeros_like(p) for p in params]
 
+# Kolla igenom denna?
+# vad används idx för, vad är i?
+# Måste p loopas igenom (kan man inte göra vektoroperationer)?
     def step(self):
         self.t += 1 
         idx = 0
         for p in self.params:
             g = p.grad
-            self.m[p] = self.beta1*self.m[p] + (1-self.beta1)*g 
-            self.v[p] = self.beta2*self.v[p] + (1-self.beta2)*g**2
-            m_hat = self.m[p]/(1-self.beta1**self.t)
-            v_hat = self.v[p]/(1-self.v_hat**self.t)
+            self.m[i] = self.beta1*self.m[i] + (1-self.beta1)*g 
+            self.v[i] = self.beta2*self.v[i] + (1-self.beta2)*g**2
+            m_hat = self.m[i]/(1-self.beta1**self.t)
+            v_hat = self.v[i]/(1-self.beta2**self.t)
             if self.lambd != 0: # Weight decay
                 p = p - self.alpha*self.lambd*p
             p = p - self.alpha*m_hat/(np.sqrt(v_hat)+self.eps)
