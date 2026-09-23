c6b2726773dc842c472f097a1349078d75313a47
Aron Frodin <aronfr@kth.se>
2026-09-23 14:13:25 +0200

    Adam optimizer

---
+++ src/optimizers/adam.py
@@ -0,0 +1,31 @@
+from optim import Optimizer
+
+class Adam(Optimizer):
+    '''
+    See https://arxiv.org/pdf/1412.6980 for lambd = 0 and for lambda != 0 (i.e weight decay) see https://arxiv.org/pdf/1711.05101 (we dont test the L_2 reg)
+    '''
+
+    def __init__(params, alpha, beta1, beta2, eps, lambd = 0):
+        super().__init__(params)
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.lambd = lambd
+        self.t = 0 
+        self.m = [np.zeros_like(p) for p in params]
+        self.v = [np.zeros_like(p) for p in params]
+
+    def step(self):
+        self.t += 1 
+        idx = 0
+        for p in params:
+            g = p.grad + self.lambd*p
+            self.m[i] = self.beta1*self.m[i] + (1-self.beta1)*g 
+            self.v[i] = self.beta2*self.v[i] + (1-self.beta2)*g**2
+            m_hat = self.m[i]/(1-self.beta1**t)
+            v_hat = self.v[i]/(1-self.v_hat**t)
+            if self.lambd != 0: # Weight decay
+                p = p - self.alpha*self.lambd*p
+            p = p - self.alpha*m_hat/(np.sqrt(v_hat)+self.eps)
+            idx +=1 
